fe3ccad74340bfa2043f608c193e7bf4f845c4b5
Remy GENY <rgeny@e1r2p1.clusters.42paris.fr>
2022-09-01 15:22:38 +0200

    vector move resize and reserve function in capacity.hpp

--- includes/templates/vector/capacity.hpp
+++ includes/templates/vector/capacity.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.capacity.hpp                                :+:      :+:    :+:   */
+/*   capacity.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 18:39:20 by rgeny             #+#    #+#             */
-/*   Updated: 2022/08/30 14:40:33 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/01 15:21:23 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,69 @@ bool	empty	(void) const
 {
 	return (_size == 0);
 }
+
+void	reserve	(size_type new_cap)
+{
+#ifdef FT_CONTAINER_DEBUG
+	std::cout	<< "_capacity in reserve begin = "
+				<< _capacity
+				<< std::endl
+				<< "_data in reserve begin = "
+				<< _data
+				<< std::endl;
+#endif
+
+	if (new_cap > this->max_size())
+		throw (std::length_error(FT_VECTOR_LENGTH_ERROR));
+	if (new_cap <= _capacity)
+		return ;
+
+	pointer	new_data = _alloc.allocate(new_cap);
+
+	for (size_type i = 0;
+		 i < _size;
+		 i++)
+	{
+		_alloc.construct(new_data + i, _data[i]);
+	}
+	_destroy_all();
+	_alloc.deallocate(_data, _capacity);
+	_data = new_data;
+	_capacity = new_cap;
+
+#ifdef FT_CONTAINER_DEBUG
+	std::cout	<< "_capacity in reserve end = "
+				<< _capacity
+				<< std::endl
+				<< "_data in reserve end = "
+				<< _data
+				<< std::endl;
+#endif
+}
+
+void	resize	(size_type count,
+				 value_type value = T() )
+{
+	if (count == _size)
+		return ;
+	if (count > _capacity)
+		this->reserve(ft::max(count, _size * 2));
+
+	if (_size < count)
+	{
+		for (size_type i = _size; i < count; i++)
+			_alloc.construct(_data + i, value);
+	}
+	else
+	{
+		for (size_type i = _size; i < count; i++)
+			_alloc.destroy(_data + i);
+	}
+
+	_size = count;
+}
+
+
 
 # endif
 #endif
--- includes/templates/vector/modifiers.hpp
+++ includes/templates/vector/modifiers.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 18:35:12 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/01 15:04:22 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/01 15:20:37 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,44 +20,12 @@ void	assign	(InputIterator first
 {
 	this->clear();
 	this->insert(this->begin(), first, last);
-//	this->insert(0, first, last);
-//	if (first == last) //à revoir
-//		return ;
-//	for (pointer it = _data, ite = _data + _capacity;
-//		 it < ite;
-//		 it++)
-//	{
-//		_alloc.destroy(it);
-//	}
-//	_alloc.deallocate(_data, _capacity);
-//	_size = std::distance(first, last);
-//	_capacity = _size;
-//	_data = _alloc.allocate(_size);
-//	for (size_type i = 0; first != last; i++, first++)
-//		_alloc.construct(_data + i, *first);
 }
 void	assign	(size_type n
 				,const_reference val)
 {
 	this->clear();
 	this->insert(this->begin(), n, val);
-//	this->insert(0, n, val);
-//	for (pointer it = _data, ite = _data + _size;
-//		 it < ite;
-//		 it++)
-//	{
-//		 _alloc.destroy(it);
-//	}
-//	_alloc.deallocate(_data, _size);
-//	_size = n;
-//	_capacity = n;
-//	_data = _alloc.allocate(n);
-//	for (pointer it = _data, ite = _data + _size;
-//		 it < ite;
-//		 it++)
-//	{
-//		_alloc.construct(it, val);
-//	}
 }
 
 void	clear	(void)
@@ -66,67 +34,67 @@ void	clear	(void)
 	_size = 0;
 }
 
-void	reserve	(size_type new_cap)
-{
-#ifdef FT_CONTAINER_DEBUG
-	std::cout	<< "_capacity in reserve begin = "
-				<< _capacity
-				<< std::endl
-				<< "_data in reserve begin = "
-				<< _data
-				<< std::endl;
-#endif
+//void	reserve	(size_type new_cap)
+//{
+//#ifdef FT_CONTAINER_DEBUG
+//	std::cout	<< "_capacity in reserve begin = "
+//				<< _capacity
+//				<< std::endl
+//				<< "_data in reserve begin = "
+//				<< _data
+//				<< std::endl;
+//#endif
+//
+//	if (new_cap > this->max_size())
+//		throw (std::length_error(FT_VECTOR_LENGTH_ERROR));
+//	if (new_cap <= _capacity)
+//		return ;
+//
+//	pointer	new_data = _alloc.allocate(new_cap);
+//
+//	for (size_type i = 0;
+//		 i < _size;
+//		 i++)
+//	{
+//		_alloc.construct(new_data + i, _data[i]);
+//	}
+//	_destroy_all();
+//	_alloc.deallocate(_data, _capacity);
+//	_data = new_data;
+//	_capacity = new_cap;
+//
+//#ifdef FT_CONTAINER_DEBUG
+//	std::cout	<< "_capacity in reserve end = "
+//				<< _capacity
+//				<< std::endl
+//				<< "_data in reserve end = "
+//				<< _data
+//				<< std::endl;
+//#endif
+//}
 
-	if (new_cap > this->max_size())
-		throw (std::length_error(FT_VECTOR_LENGTH_ERROR));
-	if (new_cap <= _capacity)
-		return ;
-
-	pointer	new_data = _alloc.allocate(new_cap);
-
-	for (size_type i = 0;
-		 i < _size;
-		 i++)
-	{
-		_alloc.construct(new_data + i, _data[i]);
-	}
-	_destroy_all();
-	_alloc.deallocate(_data, _capacity);
-	_data = new_data;
-	_capacity = new_cap;
-
-#ifdef FT_CONTAINER_DEBUG
-	std::cout	<< "_capacity in reserve end = "
-				<< _capacity
-				<< std::endl
-				<< "_data in reserve end = "
-				<< _data
-				<< std::endl;
-#endif
-}
-
-void	resize	(size_type count,
-				 value_type value = T() )
-{
-	if (count == _size)
-		return ;
-	if (count > _capacity)
-		this->reserve(ft::max(count, _size * 2));
-
-	if (_size < count)
-	{
-		for (size_type i = _size; i < count; i++)
-			_alloc.construct(_data + i, value);
-	}
-	else
-	{
-		for (size_type i = _size; i < count; i++)
-			_alloc.destroy(_data + i);
-	}
-
-	_size = count;
-}
-
+//void	resize	(size_type count,
+//				 value_type value = T() )
+//{
+//	if (count == _size)
+//		return ;
+//	if (count > _capacity)
+//		this->reserve(ft::max(count, _size * 2));
+//
+//	if (_size < count)
+//	{
+//		for (size_type i = _size; i < count; i++)
+//			_alloc.construct(_data + i, value);
+//	}
+//	else
+//	{
+//		for (size_type i = _size; i < count; i++)
+//			_alloc.destroy(_data + i);
+//	}
+//
+//	_size = count;
+//}
+//
 iterator	insert	(iterator pos,
 					 const_reference value)
 {
