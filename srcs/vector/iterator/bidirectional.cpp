2446a0ce73a3c965d6a3aa4a568e202dd5381958
Remy GENY <rgeny@e1r2p1.clusters.42paris.fr>
2022-09-06 19:05:12 +0200

    add test in srcs/vector/iterator/bidirectional.cpp

--- srcs/vector/iterator/bidirectional.cpp
+++ srcs/vector/iterator/bidirectional.cpp
@@ -6,45 +6,84 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 15:29:56 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/05 15:41:43 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/06 19:04:55 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Utils.hpp"
 
-#define SIZE 7
+#define SIZE 8
 
 template
 <
-	typename Iterator
+	typename Iterator,
+	typename ConstIterator
 >
-static void	_test	(Iterator src)
+static void	_test	(Iterator src_it,
+					 ConstIterator src_cit)
 {
 	TRY_CATCH
 	(
-		Iterator	it(src);
+		Iterator		it(src_it);
+		ConstIterator	cit(src_cit);
 
-		PRINT_EXECUTE(src == it);
-		PRINT_EXECUTE(*src);
+		PRINT_EXECUTE(src_it == it);
+		PRINT_EXECUTE(src_cit == it);
+		PRINT_EXECUTE(src_it == cit);
+		PRINT_EXECUTE(src_cit == cit);
+
+		PRINT_EXECUTE(*src_it);
 		PRINT_EXECUTE(*it);
+		PRINT_EXECUTE(*src_cit);
+		PRINT_EXECUTE(*cit);
+
 		PRINT_AND_EXECUTE(it--);
-		PRINT_EXECUTE(*src);
+		PRINT_EXECUTE(*src_it);
 		PRINT_EXECUTE(*it);
+		PRINT_EXECUTE(*src_cit);
+		PRINT_EXECUTE(*cit);
+		PRINT_AND_EXECUTE(cit--);
+		PRINT_EXECUTE(*src_it);
+		PRINT_EXECUTE(*it);
+		PRINT_EXECUTE(*src_cit);
+		PRINT_EXECUTE(*cit);
+
 		PRINT_AND_EXECUTE(--it);
-		PRINT_EXECUTE(src == it);
-		PRINT_EXECUTE(*src);
+		PRINT_EXECUTE(*src_it);
 		PRINT_EXECUTE(*it);
+		PRINT_EXECUTE(*src_cit);
+		PRINT_EXECUTE(*cit);
+		PRINT_AND_EXECUTE(--cit);
+		PRINT_EXECUTE(*src_it);
+		PRINT_EXECUTE(*it);
+		PRINT_EXECUTE(*src_cit);
+		PRINT_EXECUTE(*cit);
+
+		PRINT_EXECUTE(*src_it--);
+		PRINT_EXECUTE(*it--);
+		PRINT_EXECUTE(*src_cit--);
+		PRINT_EXECUTE(*cit--);
+		PRINT_EXECUTE(*src_it--);
+		PRINT_EXECUTE(*it--);
+		PRINT_EXECUTE(*src_cit--);
+		PRINT_EXECUTE(*cit--);
 	)
 }
 
 int	main	(void)
 {
-	NAMESPACE::vector<int>	v;
+	NAMESPACE::vector<int>					v;
+	NAMESPACE::vector<int>::iterator		it;
+	NAMESPACE::vector<int>::const_iterator	cit;
 
 	for (int i = 0; i < SIZE; i++)
 	{
 		v.push_back(i * 3);
 	}
-	for (size_t i = 2; i < SIZE; i++)
-		_test(v.begin() + i);
+	for (size_t i = 3; i < SIZE; i++)
+	{
+		it = v.begin() + i;
+		cit= v.begin() + i;
+		_test(it, cit);
+	}
 }
