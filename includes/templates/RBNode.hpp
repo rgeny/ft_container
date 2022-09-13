d8fab28988ec00c418c9b23a6847e0253a97cf25
Remy GENY <rgeny@e1r11p2.clusters.42paris.fr>
2022-09-13 15:06:27 +0200

    mode RBNode in RBNode.hpp

--- includes/templates/RBNode.hpp
+++ includes/templates/RBNode.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:21:53 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/10 17:25:18 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/13 15:04:20 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,147 @@
 
 namespace ft
 {
+	enum e_color
+	{
+		RED=true,
+		BLACK=false
+	};
+#define CLASS_NAME "RBNode"
 	template
 	<
-		typename Content
+		typename Value
 	>
 	class RBNode
 	{
 		public:
-			typedef true	RED;
-			typedef false	BLACK;
-			typedef Content	content_type;
+			typedef Value						value_type;
+			typedef RBNode *					pointer;
+			typedef RBNode<value_type> const *	const_pointer;
 
-			RBNode	(void)
-				:_content()
+			template
+			<
+				typename T
+			>
+			RBNode	(T & sentinel,
+					 typename ft::enable_if<ft::is_pointer<T>::value>::type = 0)
+				:_sentinel(sentinel)
+				,_value()
 				,_color(BLACK)
-			{	}
-			RBNode	(RBNode const & src)
-				:_content(src._content)
-				,_color(src._color)
-			{	}
-			~RBNode	(void)
-			{	}
-
-			RBNode &	operator=	(RBNode const & src)
+				,_left(NULL)
+				,_right(NULL)
+				,_parent(NULL)
 			{
-				_content = src._content;
-				_color = src._color;
+#ifdef __DEBUG__
+DFL_CTOR
+#endif
+			}
+			RBNode	(const_pointer & sentinel,
+					 value_type const value,
+					 const_pointer left = NULL,
+					 const_pointer right = NULL,
+					 const_pointer parent = NULL,
+					 e_color const color = RED)
+				:_sentinel(sentinel)
+				,_value(value)
+				,_color(color)
+				,_left(left)
+				,_right(right)
+				,_parent(parent)
+			{
+#ifdef __DEBUG__
+PAR_CTOR
+#endif
+			}
+			RBNode	(RBNode const & src)
+				:_sentinel(src._sentinel)
+				,_value(src._value)
+				,_color(src._color)
+				,_left(src._left)
+				,_right(src._right)
+				,_parent(src._parent)
+			{
+#ifdef __DEBUG__
+CPY_CTOR
+#endif
+			}
+			~RBNode	(void)
+			{
+#ifdef __DEBUG__
+DTOR
+#endif
+			}
+
+//			RBNode &	operator=	(RBNode const & src)
+//			{
+//				_value = src._value;
+//				_color = src._color;
+//				_left = src._left;
+//				_right = src._right;
+//				_parent = src._parent;
+//				return (*this);
+//			}
+			RBNode &	operator=	(value_type const & value)
+			{
+				_value = value;
 				return (*this);
 			}
 
+			bool	is_red		(void) const
+			{	return (this->_color);	}
+			bool	is_black	(void) const
+			{	return ( ! (this->_color) );	}
+
+			pointer	getGrandParent	(void) const
+			{
+				if (this->_parent == _sentinel)
+					return (this->_parent);
+				return (this->_parent->_parent);
+			}
+			pointer	getParent	(void) const
+			{
+				return (this->_parent);
+			}
+			pointer	getUncle	(void) const
+			{
+				pointer	GrandParent	= this->getGrandParent();
+
+				if (GrandParent == _sentinel)
+					return (GrandParent);
+				if (GrandParent->_left == this->_parent)
+					return (GrandParent->_right);
+				return (GrandParent->_left);
+			}
+			pointer	getBrother	(void) const
+			{
+				if (this->_parent == _sentinel)
+					return (this->_parent);
+				if (this->_parent->_left == this)
+					return (this->_parent->_right);
+				return (this->_parent->_left);
+			}
+			pointer &	getLeft	(void)
+			{
+				return (this->_left);
+			}
+			pointer &	getRight	(void)
+			{
+				return (this->_right);
+			}
+
+			bool	operator==	(value_type val)
+			{	return (_value == val);	}
+			bool	operator<	(value_type val)
+			{	return (_value < val);	}
+
 		private:
-			content_type	_content;
-			bool			_color;
+			pointer &		_sentinel;
+			value_type		_value;
+			e_color			_color;
+			pointer			_left;
+			pointer			_right;
+			pointer			_parent;
 	};
+#undef CLASS_NAME
 }
 
 #endif
--- includes/templates/RBTree.hpp
+++ includes/templates/RBTree.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:19:37 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/13 13:25:48 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/13 15:05:25 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,146 +17,21 @@
 
 # include "print.hpp"
 # include "type_traits.hpp"
-
-//# include "RBNode.hpp"
-
-
+# include "RBNode.hpp"
+# define CLASS_NAME "RBTree"
 
 namespace ft
 {
-	enum e_color
+	static void	_print_nl	(std::string str)
 	{
-		RED=true,
-		BLACK=false
-	};
-
-#define CLASS_NAME "RBNode"
-	template
-	<
-		typename Value
-	>
-	class RBNode
+		std::cout	<< str
+					<< std::endl;
+	}
+	static void	_print_nl	(void)
 	{
-		public:
-			typedef Value						value_type;
-			typedef RBNode *					pointer;
-			typedef RBNode<value_type> const *	const_pointer;
+		std::cout	<< std::endl;
+	}
 
-			template
-			<
-				typename T
-			>
-			RBNode	(T & sentinel,
-					 typename ft::enable_if<ft::is_pointer<T>::value>::type = 0)
-				:_sentinel(sentinel)
-				,_value()
-				,_color(BLACK)
-				,_left(NULL)
-				,_right(NULL)
-				,_parent(NULL)
-			{
-#ifdef __DEBUG__
-DFL_CTOR
-#endif
-			}
-			RBNode	(const_pointer & sentinel,
-					 value_type const value,
-					 const_pointer left,
-					 const_pointer right,
-					 const_pointer parent,
-					 e_color const color = RED)
-				:_sentinel(sentinel)
-				,_value(value)
-				,_color(color)
-				,_left(left)
-				,_right(right)
-				,_parent(parent)
-			{
-#ifdef __DEBUG__
-PAR_CTOR
-#endif
-			}
-			RBNode	(RBNode const & src)
-				:_sentinel(src._sentinel)
-				,_value(src._value)
-				,_color(src._color)
-				,_left(src._left)
-				,_right(src._right)
-				,_parent(src._parent)
-			{
-#ifdef __DEBUG__
-CPY_CTOR
-#endif
-			}
-			~RBNode	(void)
-			{
-#ifdef __DEBUG__
-DTOR
-#endif
-			}
-
-			RBNode &	operator=	(RBNode const & src)
-			{
-				_value = src._value;
-				_color = src._color;
-				_left = src._left;
-				_right = src._right;
-				_parent = src._parent;
-				return (*this);
-			}
-
-			bool	is_red		(void) const
-			{	return (this->_color);	}
-			bool	is_black	(void) const
-			{	return ( ! (this->_color) );	}
-
-			pointer	getGrandParent	(void) const
-			{
-				if (this->_parent == _sentinel)
-					return (this->_parent);
-				return (this->_parent->_parent);
-			}
-			pointer	getParent	(void) const
-			{
-				return (this->_parent);
-			}
-			pointer	getUncle	(void) const
-			{
-				pointer	GrandParent	= this->getGrandParent();
-
-				if (GrandParent == _sentinel)
-					return (GrandParent);
-				if (GrandParent->_left == this->_parent)
-					return (GrandParent->_right);
-				return (GrandParent->_left);
-			}
-			pointer	getBrother	(void) const
-			{
-				if (this->_parent == _sentinel)
-					return (this->_parent);
-				if (this->_parent->_left == this)
-					return (this->_parent->_right);
-				return (this->_parent->_left);
-			}
-			pointer	getLeft	(void) const
-			{
-				return (this->_left);
-			}
-			pointer	getRight	(void) const
-			{
-				return (this->_right);
-			}
-
-		private:
-			pointer &		_sentinel;
-			value_type		_value;
-			e_color			_color;
-			pointer			_left;
-			pointer			_right;
-			pointer			_parent;
-	};
-#undef CLASS_NAME
-#define CLASS_NAME "RBTree"
 	template
 	<
 		typename Value,
@@ -204,31 +79,112 @@ DTOR
 				return (*this);
 			}
 
-//			std::pair<iterator, bool> insert( const value_type& value )
-//			{
-//				node_type *	new_node;
-//			}
+			value_type	insert(value_type const & value )
+			{
+				if (_head == NULL)
+				{
+					node_type *	new_node = _alloc.allocate(1);
+					node_type	tmp(_sentinel, value);
+					_alloc.construct(new_node, tmp);
+					_head = new_node;
+				}
+				else
+					_insert(_head, value);
+				return (value);
+			}
 
 //			iterator insert( iterator hint, const value_type& value );
-
 
 		private:
 			allocator_type			_alloc;
 			RBNode<value_type> *	_sentinel;
 			RBNode<value_type> *	_head;
 
+
+
 			void	_delete_all	(void)
 			{
 				if (_head != NULL)
+					_delete(_head);
+			}
+			void	_delete	(RBNode<value_type> * &	node)
+			{
+				if (node != NULL)
 				{
-					_alloc.destroy(_head);
-					_alloc.deallocate(_head, 1);
-					_head = NULL;
+					_delete(node->getLeft());
+					_delete(node->getRight());
+					_alloc.destroy(node);
+					_alloc.deallocate(node, 1);
+					node = NULL;
 				}
 			}
-	};
-#undef CLASS_NAME
-}
 
+			void	_insert	(RBNode<value_type> * & cur_pt,
+							 value_type const & value)
+			{
+#ifdef __DEBUG__
+	ft::_print_nl();
+	ft::_print_nl("---_insert (RBNode<value_type> * & cur_pt, value_type const & value)---");
+#endif
+				if (*cur_pt == value)
+				{
+#ifdef __DEBUG__
+	_print_nl("*cur_pt = value");
+#endif
+					*cur_pt = value;
+				}
+				else if (*cur_pt < value)
+				{
+#ifdef __DEBUG__
+	ft::_print_nl("*cur_pt < value");
+#endif
+					if (cur_pt->getRight() == NULL)
+					{
+#ifdef __DEBUG__
+	ft::_print_nl("cur_pt->getRight() == NULL");
+#endif
+						RBNode<value_type> **	pt = &cur_pt->getRight();
+						*pt = _alloc.allocate(1);
+						RBNode<value_type>	tmp(_sentinel, value);
+						_alloc.construct(cur_pt->getRight(), tmp);
+					}
+					else
+					{
+#ifdef __DEBUG__
+	ft::_print_nl("cur_pt->getRight() != NULL");
+#endif
+						_insert(cur_pt->getRight(), value);
+					}
+				}
+				else
+				{
+#ifdef __DEBUG__
+	ft::_print_nl("*cur_pt > value");
+#endif
+					if (cur_pt->getLeft() == NULL)
+					{
+#ifdef __DEBUG__
+	ft::_print_nl("cur_pt->getLeft() == NULL");
+#endif
+						cur_pt->getLeft() = _alloc.allocate(1);
+						RBNode<value_type>	tmp(_sentinel, value);
+						_alloc.construct(cur_pt->getLeft(), tmp);
+					}
+					else
+					{
+#ifdef __DEBUG__
+	ft::_print_nl("cur_pt->getLeft() != NULL");
+#endif
+						_insert(cur_pt->getLeft(), value);
+					}
+				}
+#ifdef __DEBUG__
+	ft::_print_nl("---end---");
+	ft::_print_nl();
+#endif
+			}
+	};
+}
+# undef CLASS_NAME
 #endif
 
