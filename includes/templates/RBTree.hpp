313adb1dbee7b7c363c6f7b0a6ee418a0765ad77
Remy GENY <rgeny@e1r11p2.clusters.42paris.fr>
2022-09-13 13:28:17 +0200

    RBNode: add pointer to sentinel

--- includes/templates/RBTree.hpp
+++ includes/templates/RBTree.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:19:37 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/13 11:39:42 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/13 13:25:48 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define RBTREE_HPP
 
 # include <iostream>
+
+# include "print.hpp"
+# include "type_traits.hpp"
 
 //# include "RBNode.hpp"
 
@@ -27,6 +30,7 @@ namespace ft
 		BLACK=false
 	};
 
+#define CLASS_NAME "RBNode"
 	template
 	<
 		typename Value
@@ -34,18 +38,27 @@ namespace ft
 	class RBNode
 	{
 		public:
-			typedef Value			value_type;
-			typedef RBNode *		pointer;
-			typedef RBNode const *	const_pointer;
+			typedef Value						value_type;
+			typedef RBNode *					pointer;
+			typedef RBNode<value_type> const *	const_pointer;
 
-			RBNode	(const_pointer & sentinel)
+			template
+			<
+				typename T
+			>
+			RBNode	(T & sentinel,
+					 typename ft::enable_if<ft::is_pointer<T>::value>::type = 0)
 				:_sentinel(sentinel)
 				,_value()
 				,_color(BLACK)
 				,_left(NULL)
 				,_right(NULL)
 				,_parent(NULL)
-			{	}
+			{
+#ifdef __DEBUG__
+DFL_CTOR
+#endif
+			}
 			RBNode	(const_pointer & sentinel,
 					 value_type const value,
 					 const_pointer left,
@@ -58,7 +71,11 @@ namespace ft
 				,_left(left)
 				,_right(right)
 				,_parent(parent)
-			{	}
+			{
+#ifdef __DEBUG__
+PAR_CTOR
+#endif
+			}
 			RBNode	(RBNode const & src)
 				:_sentinel(src._sentinel)
 				,_value(src._value)
@@ -66,9 +83,17 @@ namespace ft
 				,_left(src._left)
 				,_right(src._right)
 				,_parent(src._parent)
-			{	}
+			{
+#ifdef __DEBUG__
+CPY_CTOR
+#endif
+			}
 			~RBNode	(void)
-			{	}
+			{
+#ifdef __DEBUG__
+DTOR
+#endif
+			}
 
 			RBNode &	operator=	(RBNode const & src)
 			{
@@ -123,37 +148,53 @@ namespace ft
 			}
 
 		private:
-			const_pointer &	_sentinel;
+			pointer &		_sentinel;
 			value_type		_value;
 			e_color			_color;
 			pointer			_left;
 			pointer			_right;
 			pointer			_parent;
 	};
-
+#undef CLASS_NAME
+#define CLASS_NAME "RBTree"
 	template
 	<
-		typename Value
+		typename Value,
+		typename Allocator = std::allocator<RBNode<Value> >
 	>
 	class RBTree
 	{
 		public:
 			typedef Value				value_type;
 			typedef	RBNode<value_type>	node_type;
+			typedef Allocator			allocator_type;
 
-			RBTree	(void)
-				:_sentinel(new RBNode<value_type>(_sentinel))
+			RBTree	(allocator_type const & alloc = allocator_type())
+				:_alloc(alloc)
+				,_sentinel(NULL)
 				,_head(NULL)
-			{	}
+			{
+				RBNode<value_type>	tmp(_sentinel);
+				_sentinel = _alloc.allocate(1, &tmp);
+				_alloc.construct(_sentinel, _sentinel);
+			}
 
-			RBTree	(__attribute__ ((unused)) RBTree const & src)
-				:_sentinel(new RBNode<value_type>(_sentinel))
+			RBTree	(__attribute__ ((unused)) RBTree const & src,
+					 allocator_type const & alloc = allocator_type())
+				:_alloc(alloc)
+				,_sentinel(NULL)
 				,_head(NULL)
-			{	}
+			{
+				_sentinel = _alloc.allocate(1, _sentinel);
+				_alloc.construct(_sentinel);
+				if (src._head != NULL)
+					_head = _alloc.allocate(1, *src._head);
+			}
 			~RBTree	(void)
 			{
 				_delete_all();
-				delete _sentinel;
+				_alloc.destroy(_sentinel);
+				_alloc.deallocate(_sentinel, 1);
 			}
 
 			RBTree &	operator=	(__attribute__ ((unused)) RBTree const & src)
@@ -172,18 +213,21 @@ namespace ft
 
 
 		private:
-			RBNode<value_type> const *	_sentinel;
-			RBNode<value_type> *		_head;
+			allocator_type			_alloc;
+			RBNode<value_type> *	_sentinel;
+			RBNode<value_type> *	_head;
 
 			void	_delete_all	(void)
 			{
 				if (_head != NULL)
 				{
-					delete (_head);
+					_alloc.destroy(_head);
+					_alloc.deallocate(_head, 1);
 					_head = NULL;
 				}
 			}
 	};
+#undef CLASS_NAME
 }
 
 #endif
