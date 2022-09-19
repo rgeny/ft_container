5517e97226a938481061f21ddd66b123a90229ec
Remy GENY <rgeny@e1r10p5.clusters.42paris.fr>
2022-09-19 17:26:36 +0200

    add _construct function

--- includes/templates/RBTree.hpp
+++ includes/templates/RBTree.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 17:19:37 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/19 15:30:31 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/19 17:24:57 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,46 +45,14 @@ namespace ft
 		typename Value = Key,
 		typename KeyOfValue = Value, // tmp
 		typename Compare = std::less<Value>,
-		typename Allocator = std::allocator<RBNode<Value> >
+		typename Allocator = std::allocator<Value>
 	>
 	class RBTree
 	{
 		public:
-			typedef Value					value_type;
-			typedef	RBNode<value_type>		node_type;
-			typedef	RBNode<value_type> *	node_pointer;
-			typedef Allocator				allocator_type;
-			typedef size_t					size_type;
 
-			RBTree	(allocator_type const & alloc = allocator_type())
-				:_alloc(alloc)
-				,_sentinel(NULL)
-				,_root(NULL)
-				,_size(0)
-			{
-				node_type	tmp(_sentinel);
-				_sentinel = _alloc.allocate(1, &tmp);
-				_alloc.construct(_sentinel, _sentinel);
-				_root = _sentinel;
-			}
-
-			RBTree	(__attribute__ ((unused)) RBTree const & src,
-					 allocator_type const & alloc = allocator_type())
-				:_alloc(alloc)
-				,_sentinel(_alloc.allocate(1, _sentinel))
-				,_root(_sentinel)
-				,_size(0)
-			{
-				_alloc.construct(_sentinel);
-				if (src._root != src._sentinel)
-					_root = _alloc.allocate(1, *src._root);
-			}
-			~RBTree	(void)
-			{
-				this->clear();
-				_alloc.destroy(_sentinel);
-				_alloc.deallocate(_sentinel, 1);
-			}
+# include "RBTree/typedef.hpp"
+# include "RBTree/structor.hpp"
 
 			RBTree &	operator=	(__attribute__ ((unused)) RBTree const & src)
 			{
@@ -96,12 +64,7 @@ namespace ft
 			value_type	insert(value_type const & value )
 			{
 				if (_root == _sentinel)
-				{
-					node_pointer	new_node = _alloc.allocate(1);
-					node_type		tmp(_sentinel, _sentinel, value);
-					_alloc.construct(new_node, tmp);
-					_root = new_node;
-				}
+					_construct(_root, _sentinel, value);
 				else
 					_insert(_root, value);
 				return (value);
@@ -109,30 +72,23 @@ namespace ft
 
 			void	clear	(void)
 			{
-				if (_root != _sentinel)
-					_clear(_root);
+				_clear_all(_root);
 			}
+
+			size_type	size	(void) const
+			{	return (_size);	}
 
 		private:
 
 //			iterator insert( iterator hint, const value_type& value );
 
-			allocator_type	_alloc;
-			node_pointer	_sentinel;
-			node_pointer	_root;
-			size_type		_size;
+			allocator_type			_alloc;
+			_node_allocator_type	_node_alloc;
+			node_pointer			_sentinel;
+			node_pointer			_root;
+			size_type				_size;
 
-			void	_clear	(node_pointer & node)
-			{
-				if (node != _sentinel)
-				{
-					_clear(node->left);
-					_clear(node->right);
-					_alloc.destroy(node);
-					_alloc.deallocate(node, 1);
-					node = _sentinel;
-				}
-			}
+# include "RBTree/private.hpp"
 
 			void	_balance	(node_pointer & cur_pt)
 			{
@@ -144,69 +100,22 @@ namespace ft
 			void	_insert	(node_pointer & cur_pt,
 							 value_type const & value)
 			{
-#ifdef __DEBUG__
-	ft::_print_nl();
-	ft::_print_nl("---_insert (RBNode<value_type> * & cur_pt, value_type const & value)---");
-#endif
 				if (*cur_pt == value)
-				{
-#ifdef __DEBUG__
-	_print_nl("*cur_pt = value");
-#endif
-					node_type	tmp(_sentinel, cur_pt->parent, value);
-					_alloc.destroy(cur_pt);
-					_alloc.construct(cur_pt, tmp);
-				}
+					_construct(cur_pt, cur_pt->parent, value);
 				else if (*cur_pt < value)
 				{
-#ifdef __DEBUG__
-	ft::_print_nl("*cur_pt < value");
-#endif
 					if (cur_pt->right == _sentinel)
-					{
-#ifdef __DEBUG__
-	ft::_print_nl("cur_pt->right == NULL");
-#endif
-						cur_pt->right = _alloc.allocate(1);
-						node_type	tmp(_sentinel, cur_pt, value);
-						_alloc.construct(cur_pt->right, tmp);
-						++_size;
-					}
+						_construct(cur_pt->right, cur_pt, value);
 					else
-					{
-#ifdef __DEBUG__
-	ft::_print_nl("cur_pt->right != NULL");
-#endif
 						_insert(cur_pt->right, value);
-					}
 				}
 				else
 				{
-#ifdef __DEBUG__
-	ft::_print_nl("*cur_pt > value");
-#endif
 					if (cur_pt->left == _sentinel)
-					{
-#ifdef __DEBUG__
-	ft::_print_nl("cur_pt->left == NULL");
-#endif
-						cur_pt->left = _alloc.allocate(1);
-						node_type	tmp(_sentinel, cur_pt, value);
-						_alloc.construct(cur_pt->left, tmp);
-						++_size;
-					}
+						_construct(cur_pt->left, cur_pt, value);
 					else
-					{
-#ifdef __DEBUG__
-	ft::_print_nl("cur_pt->left != NULL");
-#endif
 						_insert(cur_pt->left, value);
-					}
 				}
-#ifdef __DEBUG__
-	ft::_print_nl("---end---");
-	ft::_print_nl();
-#endif
 			}
 # include "RBTree/print.hpp"
 	};
--- includes/templates/RBTree/private.hpp
+++ includes/templates/RBTree/private.hpp
@@ -6,13 +6,29 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:07:50 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/19 17:10:20 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/19 17:20:58 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifdef RBTREE_HPP
 #ifndef RBTREE_PRIVATE_HPP
 # define RBTREE_PRIVATE_HPP
+
+void	_construct	(node_preference new_node,
+					 node_preference parent,
+					 value_type const & value = value_type() )
+{
+	if (new_node != _sentinel)
+		_node_alloc.destroy(new_node);
+	else
+	{
+		new_node = _node_alloc.allocate(1);
+		++_size;
+	}
+	
+	node_type	tmp(_sentinel, parent, value);
+	_node_alloc.construct(new_node, tmp);
+}
 
 void	_clear		(node_preference node)
 {
