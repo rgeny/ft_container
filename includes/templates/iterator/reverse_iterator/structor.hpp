e13bc78d6f2b092be60cad9571b77ca01df09acc
Remy GENY <rgeny@e1r7p3.clusters.42paris.fr>
2022-09-22 22:55:06 +0200

    debug reverse_iterator structor

--- includes/templates/iterator/reverse_iterator/structor.hpp
+++ includes/templates/iterator/reverse_iterator/structor.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 14:47:50 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/09 15:13:30 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/09/22 22:54:50 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,38 @@
 
 reverse_iterator	(void)
 	:current()
-{	}
+{
+#ifdef __DEBUG__
+DFL_CTOR
+#endif
+}
 
 explicit reverse_iterator	(iterator_type	src)
 	:current(src)
-{	}
-
-template
-<
-	typename Iter
->
-reverse_iterator	(reverse_iterator<Iter> const & src)
-	:current(src.base())
-{	}
+{
+#ifdef __DEBUG__
+PAR_CTOR
+#endif
+}
 
 reverse_iterator	(reverse_iterator const & src)
-	:current(src.current)
-{	}
+	:current(src.base())
+{
+#ifdef __DEBUG__
+CPY_CTOR
+#endif
+}
+
+template < typename Iter >
+reverse_iterator	(reverse_iterator<Iter> const & src)
+	:current(src.base())
+{
+#ifdef __DEBUG__
+std::cout	<< CLASS_NAME " const cpy constructor."
+			<< std::endl;
+#endif
+}
+
 
 # endif
 #endif
