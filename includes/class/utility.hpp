6698c97dee61b99195a94e4647d05958496f1d1b
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 19:55:08 +0200

    string.hpp: rename to utility/to_string.hpp and add in utility.hpp

--- includes/class/utility.hpp
+++ includes/class/utility.hpp
@@ -6,17 +6,21 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 19:31:07 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/18 19:32:27 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 19:44:25 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILITY_HPP
 # define UTILITY_HPP
 
+# include <ostream>
+# include <sstream>
+# include "type_traits.hpp"
+
 namespace ft
 {
 # include "utility/pair.hpp"
-
+# include "utility/to_string.hpp"
 };
 
 #endif
--- includes/class/utility/to_string.hpp
+++ includes/class/utility/to_string.hpp
@@ -1,37 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string.hpp                                         :+:      :+:    :+:   */
+/*   to_string.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 10:22:46 by rgeny             #+#    #+#             */
-/*   Updated: 2022/09/06 15:07:46 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 19:44:28 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRING_HPP
-# define STRING_HPP
+#ifdef UTILITY_HPP
+# ifndef UTILITY_TO_STRING_HPP
+#  define UTILITY_TO_STRING_HPP
 
-# include <ostream>
-# include <sstream>
-
-# include "type_traits.hpp"
-
-namespace ft
+template < typename T >
+std::string	to_string	(T value,
+						 typename ft::enable_if<is_integral<T>::value>::type = 0)
 {
-	template
-	<
-		typename T
-	>
-	std::string	to_string	(T value,
-							 typename ft::enable_if<is_integral<T>::value>::type = 0)
-	{
-		std::ostringstream	oss;
-		oss	<< value;
-		return (oss.str());
-	}
+	std::ostringstream	oss;
+	oss	<< value;
+	return (oss.str());
 }
 
+# endif
 #endif
 
